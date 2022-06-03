da20e113afbcb767008b11e1717fd78655b7ef79
weijian <weijian@baidu.com>
2022-06-03 15:10:23 +0800

    init grpJ

---
+++ src/main/java/com/example/grpcj/GrpcJApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpcj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcJApplication.class, args);
+    }
+
+}
